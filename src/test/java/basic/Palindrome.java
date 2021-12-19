91343e71515ed828134a790038fcfe4e17bf39cd
Love <love@Loves-MacBook-Pro.local>
2021-12-19 14:14:35 +0530

    pattern 3

--- src/test/java/basic/Palindrome.java
+++ src/test/java/basic/Palindrome.java
@@ -5,17 +5,16 @@ public class Palindrome {
         int number = 121, temp = number;
         int rev = 0;
         int rem = 0;
-        while(number>0){
+        while (number > 0) {
             rem = number % 10;
             rev = rev * 10 + rem;
             number /= 10;
         }
-        System.out.println("Reversed number is : "+rev);
+        System.out.println("Reversed number is : " + rev);
 
-        if(temp == rev){
+        if (temp == rev) {
             System.out.println("Number is palindrome");
-        }
-        else{
+        } else {
             System.out.println("Number is not palindrome ");
         }
 
