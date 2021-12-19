032b077e72715399c196619124855d5af134aba9
Love <love@Loves-MacBook-Pro.local>
2021-12-19 11:05:23 +0530

    factorial

---
+++ src/test/java/basic/Factorial.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 6,fact = 1;
+        for(int i=1;i<=num;i++){
+            fact = fact * i;
+        }
+        System.out.println(fact);
+    }
+}
