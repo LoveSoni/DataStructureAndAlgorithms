f639a1de96210d3d29154f70cbc1cf26d3f360cd
Love <love@Loves-MacBook-Pro.local>
2021-12-19 11:42:25 +0530

    armstrong number

---
+++ src/test/java/basic/ArmstrongNumber.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int a = 0, b = 1, c=0, till = 10;
+        System.out.print(a+" ");
+        System.out.print(b+" ");
+        for(int i=1;a+b<till;i++){
+            c = a+b;
+            System.out.print(c+" ");
+            a = b;
+            b = c;
+        }
+    }
+}
