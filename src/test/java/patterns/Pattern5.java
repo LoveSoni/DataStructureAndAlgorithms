7f1249129e6cdf7c57be069b8ef83423ce98acbd
Love <love@Loves-MacBook-Pro.local>
2021-12-19 16:31:43 +0530

    new patterns

--- src/test/java/patterns/Pattern5.java
+++ src/test/java/patterns/Pattern5.java
@@ -1,11 +1,17 @@
 package patterns;
 
 public class Pattern5 {
+    /*
+    1 2 3
+    4 5 6
+    7 8 9
+     */
     public static void main(String[] args) {
         int sum =1 ;
-        for(int i=1;i<=3;i++){
-            for(int j=1;j<=3;j++){
-                System.out.print(sum++);
+        int num = 5;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print(sum+++" ");
             }
             System.out.println();
         }
