aa9a83d9b1926a8173709a25de58200b650fc183
Love <love@Loves-MacBook-Pro.local>
2021-12-20 14:50:57 +0530

    fibonacci series

--- src/test/java/basic/Fibonacci.java
+++ src/test/java/basic/Fibonacci.java
@@ -1,6 +1,6 @@
 package basic;
 
-public class ArmstrongNumber {
+public class Fibonacci {
     public static void main(String[] args) {
         int a = 0, b = 1, c=0, till = 10;
         System.out.print(a+" ");
