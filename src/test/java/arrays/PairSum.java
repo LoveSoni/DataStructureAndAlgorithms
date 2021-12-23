fce6e670a505b00566443485af5fa0071ead83f2
Love <lovesoni607@gmail.com>
2021-12-23 17:46:13 +0530

    sum paris

---
+++ src/test/java/arrays/PairSum.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class PairSum {
+    /*
+    Array and sum is given
+    array = {1,2,3,4,5}
+    sum = 5
+    we have to find the pairs from the above array
+     */
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+        int sum = 5;
+        for(int i=0;i<a.length;i++){
+            for(int j= i+1;j<a.length;j++){
+                if(a[i]+a[j] == sum){
+                    System.out.println("Pairs {"+a[i]+","+a[j]+"}");
+                }
+            }
+        }
+    }
+}
