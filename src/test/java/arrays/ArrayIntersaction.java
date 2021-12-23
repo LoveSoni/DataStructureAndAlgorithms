9c2eeda6e3299038504355f17e7c14c675533680
Love <lovesoni607@gmail.com>
2021-12-23 16:35:09 +0530

    Array Intersaction

--- src/test/java/arrays/ArrayIntersaction.java
+++ src/test/java/arrays/ArrayIntersaction.java
@@ -1,4 +1,22 @@
 package arrays;
 
 public class ArrayIntersaction {
+    /*
+    {1,2,3}
+    {3,4}
+     output : 3, because it is common
+     */
+    public static void main(String[] args) {
+        int[] firstArray =  {1,2,3};
+        int[] secondArray = {3,4};
+
+        for(int i=0;i<firstArray.length;i++){
+            for(int j=0;j<secondArray.length;j++){
+                if(firstArray[i] == secondArray[j]){
+                    System.out.println(firstArray[i]);
+                    secondArray[j] = Integer.MIN_VALUE;
+                }
+            }
+        }
+    }
 }
