4d3d7a3f431a78fe4c47e31987d6fc04fd97a9b4
Love <lovesoni607@gmail.com>
2021-12-26 12:04:46 +0530

    First and last occurence

--- src/test/java/binarysearch/FirstAndLastOccurrence.java
+++ src/test/java/binarysearch/FirstAndLastOccurrence.java
@@ -8,25 +8,52 @@ public class FirstAndLastOccurrence {
         First index = 1
         Last index = 2
      */
-    public static void main(String[] args) {
-        int[]a = {0,1,1,5};
-        int key = 5;
+
+    public static void firstOccurrenceOfElement(int []evenArray,int key){
+        int matchIndex = -1;
+        int start = 0;
+        int end = evenArray.length - 1;
+        int mid = (start + end) / 2;
+        for (int i = 0; start <= end; i++) {
+            if (key == evenArray[mid]) {
+                matchIndex = mid;
+                end = mid -1;
+            }
+            if (key > evenArray[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            mid = (start + end) / 2;
+        }
+        System.out.println("First occurence of "+key+" is at index "+matchIndex);
+    }
+
+
+    public static void lastOccurrenceOfElement(int []a,int key){
         int index = -1;
-        int start=0;
+        int start = 0;
         int end = a.length-1;
         int mid = (start+end)/2;
-        for(int i=0;start<=end;i++){
+        while(start<=end){
             if(key == a[mid]){
                 index = mid;
+                start = mid+1;
             }
-            if(key>mid){
-                start = mid +1;
+            else if(key>a[mid]){
+                start = mid+1;
             }
             else{
-                end = mid-1;
+                end = mid -1;
             }
             mid = (start+end)/2;
         }
-        System.out.println(index);
+        System.out.println("Last occurrence of "+key +" is at index "+index);
+    }
+
+    public static void main(String[] args) {
+        int[]a = {0,1,1,5};
+        firstOccurrenceOfElement(a,1);
+        lastOccurrenceOfElement(a,1);
     }
 }
