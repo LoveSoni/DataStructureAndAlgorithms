4ecc91d80cdcacbdebe682f75747569f7e8ecaef
Love <lovesoni607@gmail.com>
2021-12-27 19:19:56 +0530

    Remove adjacent duplicates

---
+++ src/test/java/CharArraysOrString/RemoveAdjacentDuplicates.java
@@ -0,0 +1,27 @@
+package CharArraysOrString;
+
+
+public class RemoveAdjacentDuplicates {
+    public static void main(String[] args) {
+        String string = "abbaca";
+        char[] ch = string.toCharArray();
+        for (int i = 0; i + 1 < ch.length; i++) {
+            if (ch[i] == ch[i + 1]) {
+                ch = constructArray(ch, i, i + 1);
+                i = -1;
+            }
+        }
+        System.out.println(new String(ch));
+    }
+
+
+    public static char[] constructArray(char[] ch, int firstIndex, int secondIndex) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < ch.length; i++) {
+            if (!(i == firstIndex || i == secondIndex)) {
+                sb.append(ch[i]);
+            }
+        }
+        return sb.toString().toCharArray();
+    }
+}
