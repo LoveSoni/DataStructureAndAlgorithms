21542f9b848ab270ef77304107bc191105b98bc4
Love <lovesoni607@gmail.com>
2021-12-27 15:46:37 +0530

    occurrence of character

--- src/test/java/CharArraysOrString/OccurrenceOfCharacter.java
+++ src/test/java/CharArraysOrString/OccurrenceOfCharacter.java
@@ -1,2 +1,23 @@
-package CharArraysOrString;public class OccurrenceOfCharacter {
+package CharArraysOrString;
+
+import java.util.HashMap;
+
+public class OccurrenceOfCharacter {
+    public static void main(String[] args) {
+        String str = "love soni";
+        char ch[] = str.toCharArray();
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for(int i=0;i<ch.length;i++){
+            if(map.get(ch[i])!=null){
+                int value = map.get(ch[i]);
+                ++value;
+                map.put(ch[i],value);
+            }
+            else{
+                map.put(ch[i],1);
+            }
+        }
+        System.out.println(map);
+    }
 }
