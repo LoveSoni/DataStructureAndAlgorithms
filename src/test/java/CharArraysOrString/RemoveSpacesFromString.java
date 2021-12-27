4b47a2a4203417a4d2b38f0e4596b11cba3785e3
Love <lovesoni607@gmail.com>
2021-12-27 16:10:23 +0530

    remove white spaces from string

---
+++ src/test/java/CharArraysOrString/RemoveSpacesFromString.java
@@ -0,0 +1,15 @@
+package CharArraysOrString;
+
+public class RemoveSpacesFromString {
+    public static void main(String[] args) {
+        String string = "my name is love soni";
+        char[] charArray = string.toCharArray();
+        for(int i=0;i<charArray.length;i++){
+            if(charArray[i]==' '){
+                charArray[i] = '@';
+            }
+        }
+        string = new String(charArray);
+        System.out.println(string);
+    }
+}
