85b98d67a81439b6b4d472cadcc6da31a7a4effa
Love <lovesoni607@gmail.com>
2021-12-28 23:46:10 +0530

    row and column wise sum

--- src/test/java/twodimensionalpackage/ColumnWiseSum.java
+++ src/test/java/twodimensionalpackage/ColumnWiseSum.java
@@ -1,2 +1,15 @@
-package twodimensionalpackage;public class ColumnWiseSum {
+package twodimensionalpackage;
+
+public class ColumnWiseSum {
+    public static void main(String[] args) {
+        int[][] a = {{3,4,11},{2,12,1},{7,8,7}};
+        int sum = 0;
+        for(int i=0;i<a[0].length;i++){
+            sum =0;
+            for(int j=0;j<a.length;j++){
+                sum += a[j][i];
+            }
+            System.out.println("Sum of "+(i+1)+" row is : "+sum);
+        }
+    }
 }
--- src/test/java/twodimensionalpackage/RowWiseSum.java
+++ src/test/java/twodimensionalpackage/RowWiseSum.java
@@ -1,2 +1,15 @@
-package twodimensionalpackage;public class RowWiseSum {
+package twodimensionalpackage;
+
+public class RowWiseSum {
+    public static void main(String[] args) {
+        int[][] a = {{3,4,11},{2,12,1},{7,8,7}};
+        int sum = 0;
+        for(int i=0;i<a.length;i++){
+            sum = 0;
+            for(int j=0;j<a[0].length;j++){
+                sum += a[i][j];
+            }
+            System.out.println("Sum of "+i+1+" row is : "+sum);
+        }
+    }
 }
