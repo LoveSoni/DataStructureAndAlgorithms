8e013c64ff74a61e0b32f7b8599208d68a551d04
Love <lovesoni607@gmail.com>
2022-01-02 17:25:03 +0530

    power of 2

--- src/test/java/Recurssion/Power.java
+++ src/test/java/Recurssion/Power.java
@@ -1,4 +1,14 @@
 package Recurssion;
 
 public class Power {
+
+    public static int findPower(int num){
+        if(num ==0){
+            return 1;
+        }
+        return 2 * findPower(num-1);
+    }
+    public static void main(String[] args) {
+      System.out.print(findPower(5));
+    }
 }
