bf5bbbae4d28b65dd226fc2e64c458a7060ecdaa
Love <lovesoni607@gmail.com>
2022-01-03 19:00:52 +0530

    reach home

---
+++ src/test/java/Recurssion/ReachHome.java
@@ -0,0 +1,17 @@
+package Recurssion;
+
+public class ReachHome {
+
+    public static void reachHome(int src,int dest){
+        if(src > dest){
+            return;
+        }
+        System.out.println("Taking "+ src + " step");
+        src = src + 1;
+        reachHome(src,dest);
+    }
+
+    public static void main(String[] args) {
+        reachHome(1,10);
+    }
+}
--- src/test/java/twodimensionalpackage/SpiralPrint.java
+++ src/test/java/twodimensionalpackage/SpiralPrint.java
@@ -1,27 +1,43 @@
 package twodimensionalpackage;
 
 public class SpiralPrint {
-
+    /*
+    Input:  1    2   3   4
+            5    6   7   8
+            9   10  11  12
+            13  14  15  16
+    Output: 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+     */
     public static void main(String[] args) {
-        int[][] a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] a = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
         int startRow = 0;
-        int endRow = a[0].length;
-
-        for (int i = 0; i < a.length; i++) {
+        int startColumn = a[0].length - 1;
+        int endRow = a[0].length - 1;
+        int endColumn = 0;
+        //print starting row
+        for (int i = 0; i <=0; i++) {
             for (int j = 0; j < a[0].length; j++) {
-                System.out.print(a[i][j]);
+                System.out.print(a[i][j]+"\t");
             }
+            System.out.println();
         }
-        System.out.println();
-        for (int i = 0; i < a.length; i++) {
-            for (int j = a[0].length - 1; j >= 0; j--) {
-                System.out.print(a[i][j]);
+        //print ending column
+        for(int i=0;i<a[0].length;i++){
+            for(int j=0;j<a.length;j++){
+                System.out.print(a[j][i]+"\t");
             }
+            System.out.println();
         }
-        /*
-        1 2 3
-        4 5 6
-        7 8 9
-         */
+//        for (int i = 0; i < a.length; i++) {
+//            for (int j = 0; j < a[0].length; j++) {
+//                System.out.print(a[i][j]);
+//            }
+//        }
+//        System.out.println();
+//        for (int i = 0; i < a.length; i++) {
+//            for (int j = a[0].length - 1; j >= 0; j--) {
+//                System.out.print(a[i][j]);
+//            }
+//        }
     }
 }
