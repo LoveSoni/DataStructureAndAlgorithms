8377f61a18343f5e061a73b613d720dd65bd4ee9
Love <lovesoni607@gmail.com>
2022-01-03 19:42:04 +0530

    say number

--- src/test/java/Recurssion/SayDigits.java
+++ src/test/java/Recurssion/SayDigits.java
@@ -1,4 +1,21 @@
 package Recurssion;
 
 public class SayDigits {
+    /*
+    e.g. 4 1 2
+        four one two
+     */
+    static String numberMappings[] = {"zero","one","two","three","four","five","six","seven","eight","nine","ten"};
+    public static void sayDigit(int number){
+        if(number == 0){
+            return;
+        }
+        int digit = number % 10;
+        number = number/10;
+        sayDigit(number);
+        System.out.print(numberMappings[digit]+" ");
+    }
+    public static void main(String[] args) {
+        sayDigit(412);
+    }
 }
