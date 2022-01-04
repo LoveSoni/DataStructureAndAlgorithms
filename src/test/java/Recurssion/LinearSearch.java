343045ee0d17df7fcd68c502b82f359e7cbbf9e5
Love <lovesoni607@gmail.com>
2022-01-04 19:10:35 +0530

    sum of array and binary search

---
+++ src/test/java/Recurssion/LinearSearch.java
@@ -0,0 +1,23 @@
+package Recurssion;
+
+public class LinearSearch {
+
+    public static boolean searchElement(int[] array, int key){
+        if(array.length == 0){
+            return false;
+        }
+        if(array[0] == key){
+            return true;
+        }
+        else {
+            int[] arrayNew = new int[array.length - 1];
+            System.arraycopy(array, 1, arrayNew, 0, arrayNew.length);
+            return searchElement(arrayNew,key);
+        }
+    }
+
+    public static void main(String[] args) {
+        int array[] = {1,4,5,6,8};
+        System.out.println(searchElement(array,14));
+    }
+}
---
+++ src/test/java/Recurssion/SumOfArray.java
@@ -0,0 +1,22 @@
+package Recurssion;
+
+public class SumOfArray {
+    // find the sum of array using recursion
+    public static int arraySum(int array[]) {
+        if (array.length == 0) {
+            return 0;
+        }
+        if (array.length == 1) {
+            return array[0];
+        }
+        int[] arrayNew = new int[array.length - 1];
+        System.arraycopy(array, 1, arrayNew, 0, arrayNew.length);
+        int sum = array[0] + arraySum(arrayNew);
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4, 3, 7, 3};
+        System.out.println(arraySum(array));
+    }
+}
