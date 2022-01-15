b14e06ff735d81474eda28b162df5a7f3b25eca1
Love <lovesoni607@gmail.com>
2022-01-15 18:05:34 +0530

    Prime number with Sieve of Eratosthenes algo

---
+++ src/test/java/basic/PrimeNumber_SieveOfEratosThenes_Algo.java
@@ -0,0 +1,34 @@
+package basic;
+
+import java.util.Arrays;
+
+public class PrimeNumber_SieveOfEratosThenes_Algo {
+
+    public static void main(String[] args) {
+      int till = 5000000;
+      boolean res[] = primeNumber(till);
+      for(int i=0;i<res.length;i++){
+          if(res[i]){
+              System.out.println(i);
+          }
+      }
+
+    }
+
+    public static boolean[] primeNumber(int number){
+        boolean[] allNumbers = new boolean[number+1];
+        // by default mark all number as prime i.e. true
+        Arrays.fill(allNumbers,true);
+        // since 0 and 1 is not prime mark them as false
+        allNumbers[0]=allNumbers[1] = false;
+
+        for(int i=2;i<number;i++){
+            if(allNumbers[i]){
+                for(int j=2*i;j<number;j=j+i){
+                    allNumbers[j] = false;
+                }
+            }
+        }
+        return allNumbers;
+    }
+}
